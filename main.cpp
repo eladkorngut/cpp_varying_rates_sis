c639d99f42295938d77e0aa2976a38ba02607d6e
eladkorngut <elad.korngut@mail.huji.ac.il>
2024-12-19 18:18:48 +0200

    Minor change in the rates, removed the homogenous case and instead using rates for the well-mixed case,i.e, no degree

--- main.cpp
+++ main.cpp
@@ -39,9 +39,12 @@ double gillespie1d(double tau,std::mt19937 &gen,std::uniform_real_distribution<d
         r1 = uniform_dist(gen);
         r2 = -std::log(uniform_dist(gen));
         if (time > cat_start and time <= cat_start + cat_duration)
-            rates = Num_inf * Alpha + (beta_cat * k * (N - Num_inf) * Num_inf)/N;
+//            rates = Num_inf * Alpha + (beta_cat * k * (N - Num_inf) * Num_inf)/N;
+            rates = Num_inf * Alpha + (beta_cat * (N - Num_inf) * Num_inf);
         else
-            rates = Num_inf * Alpha + (beta * k * (N - Num_inf) * Num_inf)/N;
+//            rates = Num_inf * Alpha + (beta * k * (N - Num_inf) * Num_inf)/N;
+            rates = Num_inf * Alpha + (beta * (N - Num_inf) * Num_inf);
+
         if (Alpha*Num_inf/rates<r1)
             Num_inf++;
         else
