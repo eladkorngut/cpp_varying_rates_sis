801e2d49ca9a02928179530b8bbcd16463db3c69
eladkorngut <elad.korngut@mail.huji.ac.il>
2025-02-16 09:06:21 +0200

    Changed so the program will run twice, one for the set of parameters and the other for phi=0.0 which will be a normalizing factor. In the previous version beacuse the epsilon of the graph is created differently for each network normalization was impossible

--- ensemblemaster.py
+++ ensemblemaster.py
@@ -8,34 +8,34 @@ if __name__ == '__main__':
     # Netwrok parameters
 
     # N = [300,400,500,600,700,800,900,1000,1100,1200,1300,1400]
-    N = 800
-    prog = 'complete'
-    lam = 1.2
+    N = 700
+    prog = 'gam'
+    lam = 1.24
     # lam = 1+np.logspace(-2,0,9)
     # lam = np.array([1.5,1.6,1.7,1.8])
-    measurements = 100000
     # eps_din = np.random.uniform(0.0, 3.0,measurements)
     # eps_din = [0.0, 0.05, 0.1, 0.15, 0.2]
-    eps_din = 0.0
+    eps_din = np.linspace(0.01,1.0,100)
     eps_dout = eps_din
+    measurements = np.where(eps_din < 0.2, 1000000, 10000)
     # correlation = [-0.01,-0.03,-0.05,-0.08,-0.1,-0.12,-0.15,-0.18,-0.2,-0.25,-0.3]
     correlation = 0.0
     number_of_networks = 10
     # k = [50]
-
-    k= 799
+    k= 50
 
     # Catstrophe parameters parameters
     relaxation_time = 20
-    sims = int(measurements/number_of_networks)
-    tau = 150
+    sims = (measurements/number_of_networks).astype(int)
+    tau = 100
     # tau = np.linspace(0.1,2.0,20)
-    start = 80
-    phi = 0.05
-    # duartion = 1.0
-    duartion = [0.0,2.5,5.0,7.5,10.0,12.5,15.0,17.5,20.0]
+    start = 50
+    phi = np.linspace(0.01,1.0,100)
+    duartion = 2.0
+    # duartion = [0.0,2.5,5.0,7.5,10.0,12.5,15.0,17.5,20.0]
 
-    strength = 1.0-phi
+    # strength = 1.0-phi
+    strength = np.ones(len(phi)) - phi
     error_graphs = False
 
     # Parameters that don't change
@@ -47,16 +47,23 @@ if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath(__file__))
     slurm_path = dir_path + '/slurm.serjob python3'
     program_path = dir_path + '/runwesim.py'
-    loop_over = duartion
+    loop_over = eps_din
     run_mc_simulation = False
 
-    for i in loop_over:
-        error_graphs_flag = '--error_graphs' if error_graphs else ''
-        run_mc_simulation_flag = '--run_mc_simulation' if run_mc_simulation else ''
-        short_flag_flag = False
-        command = (f'{slurm_path} {program_path} --N {N} --prog {prog} --lam {lam} --eps_din {eps_din} '
-                   f'--eps_dout {eps_dout} --correlation {correlation} --number_of_networks {number_of_networks} '
-                   f'--k {k} {error_graphs_flag} --sims {sims} --tau {tau} --start {start} --duartion {i} '
-                   f'--strength {strength} --relaxation_time {relaxation_time} --x {x} '
-                   f'--Alpha {Alpha} {run_mc_simulation_flag}')
-        os.system(command)
+    for s in strength:
+        for i,j in zip(loop_over,sims):
+            error_graphs_flag = '--error_graphs' if error_graphs else ''
+            run_mc_simulation_flag = '--run_mc_simulation' if run_mc_simulation else ''
+            short_flag_flag = False
+            command = (f'{slurm_path} {program_path} --N {N} --prog {prog} --lam {lam} --eps_din {i} '
+                       f'--eps_dout {i} --correlation {correlation} --number_of_networks {number_of_networks} '
+                       f'--k {k} {error_graphs_flag} --sims {sims} --tau {tau} --start {start} --duartion {duartion} '
+                       f'--strength {s} --relaxation_time {relaxation_time} --x {x} '
+                       f'--Alpha {Alpha} {run_mc_simulation_flag}')
+            os.system(command)
+            command = (f'{slurm_path} {program_path} --N {N} --prog {prog} --lam {lam} --eps_din {i} '
+                       f'--eps_dout {i} --correlation {correlation} --number_of_networks {number_of_networks} '
+                       f'--k {k} {error_graphs_flag} --sims {sims} --tau {tau} --start {start} --duartion {duartion} '
+                       f'--strength {0.0} --relaxation_time {relaxation_time} --x {x} '
+                       f'--Alpha {Alpha} {run_mc_simulation_flag}')
+            os.system(command)
