A fix for the way normalization is being done. Instead of running the program with two sepreate commands from ensemblemaster.py which does not create the same network, runwesim.py will run the program provided that a flag in ensamblemaster.py is raised

ensemblemaster.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     # lam = np.array([1.5,1.6,1.7,1.8])
     # eps_din = np.random.uniform(0.0, 3.0,measurements)
     # eps_din = [0.0, 0.05, 0.1, 0.15, 0.2]
-    eps_din = np.linspace(0.01,1.0,100)
+    eps_din = np.linspace(0.01,1.0,2)
     eps_dout = eps_din
     measurements = np.where(eps_din < 0.2, 1000000, 10000)
     # correlation = [-0.01,-0.03,-0.05,-0.08,-0.1,-0.12,-0.15,-0.18,-0.2,-0.25,-0.3]
@@ -30,13 +30,19 @@ if __name__ == '__main__':
     tau = 100
     # tau = np.linspace(0.1,2.0,20)
     start = 50
-    phi = np.linspace(0.01,1.0,100)
+    phi = np.linspace(0.01,1.0,2)
     duartion = 2.0
     # duartion = [0.0,2.5,5.0,7.5,10.0,12.5,15.0,17.5,20.0]
 
     # strength = 1.0-phi
     strength = np.ones(len(phi)) - phi
     error_graphs = False
+
+    # Set this flag as needed:
+    normalization_run = True  # Set to True if you want normalization to run
+
+    # Create the flag string based on the value of normalization_run
+    normalization_run_flag = '--normalization_run' if normalization_run else ''
 
     # Parameters that don't change
 
@@ -51,19 +57,14 @@ if __name__ == '__main__':
     run_mc_simulation = False
 
     for s in strength:
-        for i,j in zip(loop_over,sims):
+        for i, j in zip(loop_over, sims):
             error_graphs_flag = '--error_graphs' if error_graphs else ''
             run_mc_simulation_flag = '--run_mc_simulation' if run_mc_simulation else ''
-            short_flag_flag = False
-            command = (f'{slurm_path} {program_path} --N {N} --prog {prog} --lam {lam} --eps_din {i} '
-                       f'--eps_dout {i} --correlation {correlation} --number_of_networks {number_of_networks} '
-                       f'--k {k} {error_graphs_flag} --sims {sims} --tau {tau} --start {start} --duartion {duartion} '
-                       f'--strength {s} --relaxation_time {relaxation_time} --x {x} '
-                       f'--Alpha {Alpha} {run_mc_simulation_flag}')
-            os.system(command)
-            command = (f'{slurm_path} {program_path} --N {N} --prog {prog} --lam {lam} --eps_din {i} '
-                       f'--eps_dout {i} --correlation {correlation} --number_of_networks {number_of_networks} '
-                       f'--k {k} {error_graphs_flag} --sims {sims} --tau {tau} --start {start} --duartion {duartion} '
-                       f'--strength {0.0} --relaxation_time {relaxation_time} --x {x} '
-                       f'--Alpha {Alpha} {run_mc_simulation_flag}')
+            command = (
+                f'{slurm_path} {program_path} --N {N} --prog {prog} --lam {lam} --eps_din {i} '
+                f'--eps_dout {i} --correlation {correlation} --number_of_networks {number_of_networks} '
+                f'--k {k} {error_graphs_flag} --sims {j} --tau {tau} --start {start} --duartion {duartion} '
+                f'--strength {s} --relaxation_time {relaxation_time} --x {x} '
+                f'--Alpha {Alpha} {run_mc_simulation_flag} {normalization_run_flag}'
+            )
             os.system(command)

runwesim.py
@@ -67,10 +67,10 @@ def job_to_cluster(foldername,parameters,Istar,prog):
     os.mkdir(foldername)
     os.chdir(foldername)
     data_path = os.getcwd() +'/'
-    N, sims, start, k, x, lam, duartion, Num_inf, Alpha, number_of_networks, tau, eps_din, eps_dout, strength, prog, Beta_avg, error_graphs = parameters
-    N, sims, start, k, x, lam, duartion, Num_inf, Alpha, number_of_networks, tau, eps_din, eps_dout, strength, prog, Beta_avg, error_graphs=\
+    N, sims, start, k, x, lam, duartion, Num_inf, Alpha, number_of_networks, tau, eps_din, eps_dout, strength, prog, Beta_avg, error_graphs,normalization_run = parameters
+    N, sims, start, k, x, lam, duartion, Num_inf, Alpha, number_of_networks, tau, eps_din, eps_dout, strength, prog, Beta_avg, error_graphs, normalization_run=\
     int(N),int(sims),float(start),float(k),float(x),float(lam),float(duartion),int(Num_inf),float(Alpha),int(number_of_networks),float(tau),float(eps_din),float(eps_dout),\
-    float(strength),prog,float(Beta_avg),bool(error_graphs)
+    float(strength),prog,float(Beta_avg),bool(error_graphs),bool(normalization_run)
     G = rand_networks.configuration_model_undirected_graph_mulit_type(k,eps_din,N,prog)
     graph_degrees = np.array([G.degree(n) for n in G.nodes()])
     k_avg_graph,graph_std,graph_skewness = np.mean(graph_degrees),np.std(graph_degrees),skew(graph_degrees)
@@ -111,6 +111,14 @@ def job_to_cluster(foldername,parameters,Istar,prog):
         path_parameters = data_path + 'cparameters_{}.txt'.format(i)
         parameters_path = '{} {} {}'.format(path_adj_in,path_adj_out,path_parameters)
         os.system('{} {} {}'.format(slurm_path,program_path,parameters_path))
+
+        if normalization_run:
+            parameters_normalization = np.array([N, sims, start, k_avg_graph, x, lam, Alpha, Beta, i, tau, Istar,
+                            strength, prog, dir_path, eps_graph,eps_graph, duartion, Beta, graph_std, graph_skewness,
+                            third_moment, second_moment])
+            parameters_path_norm = '{} {} {}'.format(path_adj_in, path_adj_out, parameters_normalization)
+            os.system('{} {} {}'.format(slurm_path, program_path, parameters_path_norm))
+
         # os.system('{} {} {} {}'.format(program_path,path_adj_in,path_adj_out,path_parameters))
 
 # def job_to_cluster(foldername,parameters,Istar,prog):
@@ -260,8 +268,11 @@ if __name__ == '__main__':
     parser.add_argument('--Alpha', type=float, help='Recovery rate')
     parser.add_argument('--run_mc_simulation', action='store_true', help='Flag to run MC simulation')
     parser.add_argument('--short_path', action='store_true', help='Flag to measure mean shortest path')
+    parser.add_argument('--normalization_run', action='store_true', help='Flag for running parameter normalization')
+
 
     args = parser.parse_args()
+    normalization_run = args.normalization_run
 
     # Default parameters
     N = 1000 if args.N is None else args.N
@@ -273,6 +284,7 @@ if __name__ == '__main__':
     number_of_networks = 1 if args.number_of_networks is None else args.number_of_networks
     k = 50 if args.k is None else args.k
     error_graphs = args.error_graphs
+    normalization_run = args.normalization_run
 
     sims = 1000 if args.sims is None else args.sims
     tau = 150 if args.tau is None else args.tau
@@ -288,7 +300,8 @@ if __name__ == '__main__':
     # run_mc_simulationtion = True
 
 
-    parameters = np.array([N, sims, start, k, x, lam, duartion, Num_inf, Alpha, number_of_networks, tau, eps_din, eps_dout, strength, prog, Beta_avg, error_graphs])
+    parameters = np.array([N, sims, start, k, x, lam, duartion, Num_inf, Alpha, number_of_networks, tau, eps_din,
+                           eps_dout, strength, prog, Beta_avg, error_graphs,normalization_run])
     graphname = 'GNull'
     foldername = 'prog_{}_N{}_k_{}_R_{}_tau_{}_start_{}_duartion_{}_strength_{}_sims_{}_net_{}_epsin_{}_epsout_{}_err_{}'.format(
         prog, N, k, lam, tau, start, duartion, strength, sims, number_of_networks, eps_din, eps_dout, error_graphs)