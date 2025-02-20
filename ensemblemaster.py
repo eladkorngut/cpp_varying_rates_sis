Bug fix and change of the purpose of the program. The bug was that after the directory for the normalization run was created there was no tranfer to the orginial directory in which the data is stored. Also, now instead of running a heatmap, the program will run several simulation of delta P as a function of epsilon

ensemblemaster.py
@@ -8,30 +8,32 @@ if __name__ == '__main__':
     # Netwrok parameters
 
     # N = [300,400,500,600,700,800,900,1000,1100,1200,1300,1400]
-    N = 700
+    N = 1000
     prog = 'gam'
     lam = 1.24
     # lam = 1+np.logspace(-2,0,9)
     # lam = np.array([1.5,1.6,1.7,1.8])
     # eps_din = np.random.uniform(0.0, 3.0,measurements)
     # eps_din = [0.0, 0.05, 0.1, 0.15, 0.2]
-    eps_din = np.linspace(0.01,1.0,2)
+    eps_din = np.linspace(0.01,1.0,20)
     eps_dout = eps_din
-    measurements = np.where(eps_din < 0.2, 1000000, 10000)
+    # measurements = np.where(eps_din < 0.2, 1000000, 10000)
+    measurements = 1000000
     # correlation = [-0.01,-0.03,-0.05,-0.08,-0.1,-0.12,-0.15,-0.18,-0.2,-0.25,-0.3]
     correlation = 0.0
-    number_of_networks = 10
+    number_of_networks = 50
     # k = [50]
     k= 50
 
     # Catstrophe parameters parameters
     relaxation_time = 20
-    sims = (measurements/number_of_networks).astype(int)
+    # sims = (measurements/number_of_networks).astype(int)
+    sims = int(measurements/number_of_networks)
     tau = 100
     # tau = np.linspace(0.1,2.0,20)
     start = 50
     phi = np.linspace(0.01,1.0,2)
-    duartion = 2.0
+    duartion = 1.0
     # duartion = [0.0,2.5,5.0,7.5,10.0,12.5,15.0,17.5,20.0]
 
     # strength = 1.0-phi
@@ -56,15 +58,27 @@ if __name__ == '__main__':
     loop_over = eps_din
     run_mc_simulation = False
 
-    for s in strength:
-        for i, j in zip(loop_over, sims):
-            error_graphs_flag = '--error_graphs' if error_graphs else ''
-            run_mc_simulation_flag = '--run_mc_simulation' if run_mc_simulation else ''
-            command = (
-                f'{slurm_path} {program_path} --N {N} --prog {prog} --lam {lam} --eps_din {i} '
-                f'--eps_dout {i} --correlation {correlation} --number_of_networks {number_of_networks} '
-                f'--k {k} {error_graphs_flag} --sims {j} --tau {tau} --start {start} --duartion {duartion} '
-                f'--strength {s} --relaxation_time {relaxation_time} --x {x} '
-                f'--Alpha {Alpha} {run_mc_simulation_flag} {normalization_run_flag}'
-            )
-            os.system(command)
+    for i in loop_over:
+        error_graphs_flag = '--error_graphs' if error_graphs else ''
+        run_mc_simulation_flag = '--run_mc_simulation' if run_mc_simulation else ''
+        command = (
+            f'{slurm_path} {program_path} --N {N} --prog {prog} --lam {lam} --eps_din {i} '
+            f'--eps_dout {i} --correlation {correlation} --number_of_networks {number_of_networks} '
+            f'--k {k} {error_graphs_flag} --sims {sims} --tau {tau} --start {start} --duartion {duartion} '
+            f'--strength {strength} --relaxation_time {relaxation_time} --x {x} '
+            f'--Alpha {Alpha} {run_mc_simulation_flag} {normalization_run_flag}'
+        )
+        os.system(command)
+
+    # for s in strength:
+    #     for i, j in zip(loop_over, sims):
+    #         error_graphs_flag = '--error_graphs' if error_graphs else ''
+    #         run_mc_simulation_flag = '--run_mc_simulation' if run_mc_simulation else ''
+    #         command = (
+    #             f'{slurm_path} {program_path} --N {N} --prog {prog} --lam {lam} --eps_din {i} '
+    #             f'--eps_dout {i} --correlation {correlation} --number_of_networks {number_of_networks} '
+    #             f'--k {k} {error_graphs_flag} --sims {j} --tau {tau} --start {start} --duartion {duartion} '
+    #             f'--strength {s} --relaxation_time {relaxation_time} --x {x} '
+    #             f'--Alpha {Alpha} {run_mc_simulation_flag} {normalization_run_flag}'
+    #         )
+    #         os.system(command)

runwesim.py
@@ -121,14 +121,17 @@ def job_to_cluster(foldername,parameters,Istar,prog):
                 os.mkdir(norm_folder)
             # Change to the newly created normalization folder.
             os.chdir(norm_folder)
-
+            np.save('parameters_all.npy', parameters)
+            np.save('parameters_{}.npy'.format(i), parameters)
+            export_network_to_csv(G, i)
+            export_parameters_to_csv(parameters, i)
             parameters_normalization = np.array([
                 N, sims, start, k_avg_graph, x, lam, Alpha, Beta, i, tau, Istar,
                 strength, prog, dir_path, eps_graph, eps_graph, duartion, Beta,
                 graph_std, graph_skewness, third_moment, second_moment
             ])
             os.system('{} {} {}'.format(slurm_path, program_path, parameters_path))
-
+            os.chdir(data_path)
         # os.system('{} {} {} {}'.format(program_path,path_adj_in,path_adj_out,path_parameters))
 
 # def job_to_cluster(foldername,parameters,Istar,prog):
@@ -291,7 +294,7 @@ if __name__ == '__main__':
     eps_din = 0.1 if args.eps_din is None else args.eps_din
     eps_dout = 0.1 if args.eps_dout is None else args.eps_dout
     # correlation = 0.3 if args.correlation is None else args.correlation
-    number_of_networks = 1 if args.number_of_networks is None else args.number_of_networks
+    number_of_networks = 10 if args.number_of_networks is None else args.number_of_networks
     k = 50 if args.k is None else args.k
     error_graphs = args.error_graphs
     normalization_run = args.normalization_run