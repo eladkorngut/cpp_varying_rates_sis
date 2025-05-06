69ffa9ad9317606dc52aeeb6e9ab5b972234f26e
eladkorngut <elad.korngut@mail.huji.ac.il>
2025-05-06 10:50:58 +0300

    Bug fix, there was an error that in ensamblemaster appeared graph instead of graphname, which is the parameter that appears in runewesim.py

--- ensemblemaster.py
+++ ensemblemaster.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
             f'--eps_dout {eps_dout} --correlation {correlation} --number_of_networks {number_of_networks} '
             f'--k {k} {error_graphs_flag} --sims {sims} --tau {tau} --start {start} --duartion {duartion} '
             f'--strength {strength} --relaxation_time {relaxation_time} --x {x} '
-            f'--Alpha {Alpha} {run_mc_simulation_flag} {normalization_run_flag} {runheatcorrelation} --graph {infile}'
+            f'--Alpha {Alpha} {run_mc_simulation_flag} {normalization_run_flag} {runheatcorrelation} --graphname {infile}'
         )
 
         result = 1
