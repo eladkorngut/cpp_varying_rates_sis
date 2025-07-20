A minor bug fix. Forgot to add the number of networks parameters when I save the data in parameters_all for normalization runs

ensemblemaster.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     # N = [300,400,500,600,700,800,900,1000,1100,1200,1300,1400]
     N = 1000
     prog = 'bd'
-    lam = 1.75
+    lam = 1.6
     # lam = 1+np.logspace(-2,0,9)
     # lam = np.array([1.5,1.6,1.7,1.8])
     # eps_din = np.random.uniform(0.0, 3.0,measurements)

runwesim.py
@@ -224,7 +224,7 @@ def job_to_cluster(foldername,parameters,Istar,normalization_run,runheatcorrelat
             parameters_normalization_all = np.array([
                 N, sims, start, k_avg_graph, x, lam, Alpha, Beta, tau, Istar,
                 1.0, prog, dir_path, eps_graph, eps_graph, duartion, Beta,
-                graph_std, graph_skewness, third_moment, second_moment,graph_correlation])
+                graph_std, graph_skewness, third_moment, second_moment,graph_correlation,number_of_networks])
             np.save('parameters_all.npy', parameters_normalization_all)
             np.save('parameters_{}.npy'.format(i), parameters_normalization)
             export_network_to_csv(G, i)